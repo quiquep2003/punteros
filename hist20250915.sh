8c2e16ae86ede7b646256681da723548a66eb482
quiquep2003 <quiquep2003@gmail.com>
2025-09-15 19:46:39 -0300

    memoria de un historial de un inicio de repo relativamente rapido

---
+++ hist20250915.sh
@@ -0,0 +1,15 @@
+  500  cs ../
+  501  md punteros
+  502  git init
+  503  ls
+  504  git add .
+  505  git commit -m "repaso de uso de funteros en funciones"
+
+  513  gh punteros create --public
+  514  gh repo create quiquep2003/punteros --public --description "memoria de uso de punteros"
+  515  ls
+  516  git push -u origin main
+  517  git branch -M main
+
+  519  git status
+  520  history > hist20250915.sh
